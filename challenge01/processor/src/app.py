d3c20fc17336eb4132ee14e0069c96bf14a5ca68
yohelz <yohelz1397n@gmail.com>
2026-03-12 14:53:58 -0600

    Added code for generate text embedding Finished all the required fields types Added a new function the capitalize the subjets Added a new function to replace non-ASCII characters Added logic to index all documents Added new Env variables for authentication

--- challenge01/processor/src/app.py
+++ challenge01/processor/src/app.py
@@ -2,12 +2,15 @@ import json
 import os
 from pathlib import Path
 from typing import List, Dict, Any
+from unidecode  import unidecode
 
 from elasticsearch import Elasticsearch
 from sentence_splitter import SentenceSplitter
 from sentence_transformers import SentenceTransformer
 
 ELASTICSEARCH_URL = os.getenv("ELASTICSEARCH_URL", "http://elasticsearch:9200")
+ELASTIC_USER = os.getenv("ELASTIC_USER", "username")
+ELASTIC_PASS = os.getenv("ELASTIC_PASS", "password")
 INPUT_DIR = os.getenv("INPUT_DIR", "/app/input")
 INDEX_NAME = os.getenv("INDEX_NAME", "documents")
 EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "sentence-transformers/all-MiniLM-L6-v2")
@@ -29,7 +32,21 @@ def create_index(es: Elasticsearch, index_name: str) -> None:
             "properties": {
                 "doc_id": {"type": "keyword"},
                 "chunk_id": {"type": "keyword"},
-                # TODO: Complete the mapping with the required fields and types.
+                "doc_title": {"type": "text", 
+                                 "fields": {
+                                     "subjects": {"type": "keyword"}
+                                     }
+                                 },
+                "doc_description": {"type": "text"},
+                "doc_authors": {"type": "text"},
+                "doc_first_publish_year": {"type": "integer"},
+                "doc_subjects": {"type": "text", 
+                                 "fields": {
+                                     "subjects": {"type": "keyword"}
+                                     }
+                                 },
+                "doc_language": {"type": "keyword"},
+                "doc_openlibrary_url": {"type": "keyword"},
                 "embedding": {
                     "type": "dense_vector",
                     "dims": 384 # Adjust the dimensions where required.
@@ -63,22 +80,20 @@ def split_into_chunks(text: str, max_sentences: int = 5) -> List[str]:
 
 
 def generate_embedding(text: str) -> List[float]:
-    # TODO: Create the required code to generate text embeddings.
-    return None
+    embeddings = model.encode(text)
+    return embeddings.tolist()
 
 
 def proccess_documents(document: Dict[str, Any]) -> List[Dict[str, Any]]:
-    # TODO: Complete the required code to process each document:
-    # Split the document into chunks
-    # Generate an embedding for each chunk
-    # Add the embeddings to a new document along with the remaining fields
-    # Filter and replace non-ASCII characters
-    # Ensure that subjects are capitalized
-
 
     doc_id = document.get("id")
-    title = document.get("title", "")
-    description = document.get("description", "")
+    title = clean_non_ascii(document.get("title", ""))
+    description = clean_non_ascii(unidecode(document.get("description", "")))
+    authors = list(map(clean_non_ascii, document.get("authors", "")))
+    year = document.get("first_publish_year", "")
+    subjects = ensure_capitalize(document.get("subjects", ""))
+    language = document.get("language", "")
+    url = document.get("openlibrary_url", "")
 
     if not doc_id or not description:
         raise ValueError("Document must contain at least 'id' and 'description'")
@@ -91,16 +106,30 @@ def proccess_documents(document: Dict[str, Any]) -> List[Dict[str, Any]]:
         result.append({
             "doc_id": str(doc_id),
             "chunk_id": f"{doc_id}-{idx}",
-            "title": title,
-            "description": chunk,
+            "doc_title": title,
+            "doc_description": chunk,
+            "doc_authors": authors,
+            "doc_first_publish_year": year,
+            "doc_subjects": subjects,
+            "doc_language": language,
+            "doc_openlibrary_url": url,
             "embedding": embedding
         })
 
     return result
 
+def ensure_capitalize(subjects: list[str]) -> list[str]:
+    return [subject.capitalize() for subject in subjects]
+
+def clean_non_ascii(text: str) -> str:
+    return unidecode(text)
+    
 
 def index_documents(es: Elasticsearch, index_name: str, docs: List[Dict[str, Any]]) -> None:
-    # TODO: Index documents into Elasticsearch
+    
+    for doc in docs:
+        doc_id = doc.get("chunk_id")
+        es.index(index=index_name, document=doc, id=doc_id)
     return
 
 
@@ -115,14 +144,14 @@ def semantic_search(es: Elasticsearch, index_name: str, query_text: str, k: int 
             "k": k,
             "num_candidates": 10
         },
-        "_source": ["doc_id", "chunk_id", "title", "content"]
+        "_source": ["doc_id", "chunk_id", "doc_title", "content"]
     }
 
-    return es.search(index=index_name, body=body)
+    return es.search(index=index_name, body=body).body
 
 
 def main() -> None:
-    es = Elasticsearch(ELASTICSEARCH_URL)
+    es = Elasticsearch(ELASTICSEARCH_URL, basic_auth=(ELASTIC_USER, ELASTIC_PASS))
     create_index(es, INDEX_NAME)
     documents = load_json_files(INPUT_DIR)
 
